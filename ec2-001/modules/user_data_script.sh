3220d29f7cd74a33defed505dfdaed3c27df2824
arka <arkashankarpradhan@gmail.com>
2024-02-08 21:39:49 +0530

    added security grp

--- ec2-001/modules/user_data_script.sh
+++ ec2-001/modules/user_data_script.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 echo "Hello from user data!" > /home/ec2-user/user_data_output.txt
+
+# Update the package manager
+yum update -y
+
+# Install Docker
+yum install -y docker
+
+# Start the Docker service
+service docker start
+
+# Add the ec2-user to the docker group to run Docker without sudo
+sudo usermod -a -G docker ec2-user
+
+# Optional: Enable Docker to start on boot
+chkconfig docker on
--- ec2-001/security_group.tf
+++ ec2-001/security_group.tf
@@ -15,4 +15,18 @@ resource "aws_security_group" "sg001" {
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"           // Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]  // Allow traffic to any IP address
+  }
 }
